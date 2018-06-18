d6433783eb33031fc82cfa839a032f3eb50746a8
horisaki <seiji.horisaki@gmail.com>
2018-06-18 14:13:55 +0900

    トップページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  if Rails.env.development?
+    mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
